1b71892b24ffa63acac61a40291122252841a1f6
itggot-Pontus-Sundqvist <pontus.sundqvist@itggot.se>
2018-04-12 10:03:34 +0200

    Implement following, unfollowing, unliking and listing follows

--- app.rb
+++ app.rb
@@ -18,17 +18,39 @@ class App < Sinatra::Base
 		return posts
 	end
 
+	def get_name_of_user(user_id)
+		db = SQLite3::Database::new("./db/database.db")
+		db.results_as_hash = true
+		name = db.execute("SELECT name FROM users WHERE id=?", user_id)[0]["name"]
+		return name
+	end
+
+	get '/following' do
+		if session[:user_id]
+			db = SQLite3::Database::new("./db/database.db")
+			db.results_as_hash = true
+			users = db.execute("SELECT * FROM users WHERE id IN (SELECT followed_id FROM user_follows_user WHERE following_id=?)", session[:user_id])
+			slim(:follow_list, locals:{users:users})
+		else
+			redirect("/")
+		end
+	end
+
 	get '/page/:user' do
 		db = SQLite3::Database::new("./db/database.db")
 		db.results_as_hash = true
 		posts = db.execute("SELECT * FROM posts WHERE author_id IN (SELECT id FROM users WHERE name=?) ORDER BY created DESC LIMIT 10", params[:user])
 		posts = get_likes(posts)
-		slim(:user_feed, locals: {posts: posts})
+		if !posts.empty?
+			slim(:user_feed, locals: {posts: posts, user_id: posts[0]["author_id"]})
+		else
+			"User not found"
+		end
 	end
 	
 	get '/' do
 		if session[:user_id]
-			slim(:home_page)
+			slim(:home_page, locals: {name: get_name_of_user(session[:user_id])})
 		else
 			redirect('/login')
 		end
@@ -87,13 +109,58 @@ class App < Sinatra::Base
 		end
 	end
 
-	post '/like_post/:post_id' do
-		user_id = session[:user_id]
-		post_id = params[:post_id]
+	def liked(user_id, post_id)
 		db = SQLite3::Database::new("./db/database.db")
 		existing = db.execute("SELECT * FROM user_likes_post WHERE user_id=? AND post_id=?", [user_id, post_id])
+		!existing.empty?
+	end
+
+	def following(following, followed)
+		db = SQLite3::Database::new("./db/database.db")
+		existing = db.execute("SELECT * FROM user_follows_user WHERE following_id=? AND followed_id=?", [following, followed])
+		!existing.empty?
+	end
+
+	post '/like_post/:post_id' do
+		liking_user_id = session[:user_id]
+		post_id = params[:post_id]
+		db = SQLite3::Database::new("./db/database.db")
+		post_author = db.execute("SELECT name FROM users WHERE id IN (SELECT author_id FROM posts WHERE id=?)", post_id)[0][0]
+		if !liked(liking_user_id, post_id)
+			db.execute("INSERT INTO user_likes_post (user_id, post_id) VALUES (?, ?)", [liking_user_id, post_id])
+		end
+		redirect "page/#{post_author}##{post_id}"
+	end
+
+	post '/unlike_post/:post_id' do
+		liking_user_id = session[:user_id]
+		post_id = params[:post_id]
+		db = SQLite3::Database::new("./db/database.db")
+		post_author = db.execute("SELECT name FROM users WHERE id IN (SELECT author_id FROM posts WHERE id=?)", post_id)[0][0]
+		if liked(liking_user_id, post_id)
+			db.execute("DELETE FROM user_likes_post WHERE user_id=? AND post_id=?", [liking_user_id, post_id])
+		end
+		redirect "page/#{post_author}##{post_id}"
+	end
+
+	post '/follow_user/:user_id' do
+		following_id = session[:user_id]
+		followed_id = params[:user_id]
+		db = SQLite3::Database::new("./db/database.db")
+		existing = db.execute("SELECT * FROM user_follows_user WHERE following_id=? AND followed_id=?", [following_id, followed_id])
 		if existing.empty?
-			db.execute("INSERT INTO user_likes_post (user_id, post_id) VALUES (?, ?)", [user_id, post_id])
+			db.execute("INSERT INTO user_follows_user (following_id, followed_id) VALUES (?, ?)", [following_id, followed_id])
+		end
+		redirect back
+	end
+
+	post '/unfollow_user/:user_id' do
+		following_id = session[:user_id]
+		followed_id = params[:user_id]
+		db = SQLite3::Database::new("./db/database.db")
+		existing = db.execute("SELECT * FROM user_follows_user WHERE following_id=? AND followed_id=?", [following_id, followed_id])
+		if !existing.empty?
+			db.execute("DELETE FROM user_follows_user WHERE following_id=? AND followed_id=?", [following_id, followed_id])
 		end
 		redirect back
 	end
